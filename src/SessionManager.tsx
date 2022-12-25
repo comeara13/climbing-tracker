de91d3759457b9857c067c193b6a1dd885240268
comeara <comeara13@gmail.com>
2022-12-25 00:32:53 -0600

    initial reducer for sesnmgr=

--- src/SessionManager.tsx
+++ src/SessionManager.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import type { BoulderGrade } from './App'
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 import { Container, IconButton } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
 import BoulderTrack from './BoulderTrack'
@@ -29,10 +29,59 @@ type ClimbingSubSession = {
 type ClimbingSession = {
   startTime: Date
   endTime?: Date
-  SubSessions: ClimbingSubSession[]
+  subSessions: ClimbingSubSession[]
 }
 
+enum SessionActionKind {
+  'START',
+  'END',
+  'APPEND',
+}
+
+type reducerAction = {
+  type: SessionActionKind
+  payload?: ClimbingSubSession
+}
+
+function reducer(
+  state: ClimbingSession,
+  action: reducerAction
+): ClimbingSession {
+  const { type, payload } = action
+  switch (type) {
+    case SessionActionKind.APPEND:
+      if (payload) {
+        let newArray = [...state.subSessions]
+        newArray.push(payload)
+        return {
+          ...state,
+          subSessions: newArray,
+        }
+      }
+      return state
+
+    case SessionActionKind.END:
+      return {
+        ...state,
+        endTime: new Date(),
+      }
+
+    case SessionActionKind.START:
+      return {
+        startTime: new Date(),
+        subSessions: [],
+      }
+    default:
+      return state
+  }
+}
+// UI / controls
+// state -> make a reducer for climbing session
 function SessionManager() {
-  return <div className="Current"></div>
+  const [state, dispatch] = useReducer(reducer, {
+    startTime: new Date(),
+    subSessions: [],
+  })
+  return <div className="SessionManager"></div>
 }
 export default SessionManager
